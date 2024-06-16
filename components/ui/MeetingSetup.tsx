65c4c4d17a22122b8d01dd0442cddc3c5777a17e
sagarmandloi <spatel22799@gmail.com>
2024-06-16 23:34:29 +0530

    second commit

--- components/ui/MeetingSetup.tsx
+++ components/ui/MeetingSetup.tsx
@@ -1,54 +1,91 @@
-'use client'
+'use client';
+import { useEffect, useState } from 'react';
+import {
+  DeviceSettings,
+  VideoPreview,
+  useCall,
+  useCallStateHooks,
+} from '@stream-io/video-react-sdk';
 
-
-import { DeviceSettings, VideoPreview , useCall} from '@stream-io/video-react-sdk'
-import React, { useEffect, useState } from 'react'
+import Alert from '../Alert';
 import { Button } from './button';
-const MeetingSetup = ({setIsSetupComplete} : {setIsSetupComlete:(value:boolean)=>void}) => {
- const [isMicCamToggleOn,setIsMicCamToggleOn] = useState(false);
- const call=useCall();
 
- if(!call){
-  throw new Error('usecall must be used within Streamcall Component')
- }
+const MeetingSetup = ({
+  setIsSetupComplete,
+}: {
+  setIsSetupComplete: (value: boolean) => void;
+}) => {
+  // https://getstream.io/video/docs/react/guides/call-and-participant-state/#call-state
+  const { useCallEndedAt, useCallStartsAt } = useCallStateHooks();
+  const callStartsAt = useCallStartsAt();
+  const callEndedAt = useCallEndedAt();
+  const callTimeNotArrived =
+    callStartsAt && new Date(callStartsAt) > new Date();
+  const callHasEnded = !!callEndedAt;
 
-useEffect(()=>{
+  const call = useCall();
 
-  if(isMicCamToggleOn){
-    call?.camera.disable();
-    call?.microphone.disable();
-  }else{
-    call?.camera.enable();
-    call?.microphone.enable();
+  if (!call) {
+    throw new Error(
+      'useStreamCall must be used within a StreamCall component.',
+    );
   }
-},[isMicCamToggleOn , call?.camera , call.microphone])
 
+  // https://getstream.io/video/docs/react/ui-cookbook/replacing-call-controls/
+  const [isMicCamToggled, setIsMicCamToggled] = useState(false);
 
+  useEffect(() => {
+    if (isMicCamToggled) {
+      call.camera.disable();
+      call.microphone.disable();
+    } else {
+      call.camera.enable();
+      call.microphone.enable();
+    }
+  }, [isMicCamToggled, call.camera, call.microphone]);
+
+  if (callTimeNotArrived)
+    return (
+      <Alert
+        title={`Your Meeting has not started yet. It is scheduled for ${callStartsAt.toLocaleString()}`}
+      />
+    );
+
+  if (callHasEnded)
+    return (
+      <Alert
+        title="The call has been ended by the host"
+        iconUrl="/icons/call-ended.svg"
+      />
+    );
 
   return (
-    <div className='flex h-screen w-full flex-col items-center justify-center gap-3 text-white'>
-        <h1 className='text-2xl font-bold '>SetUp</h1>
-        <VideoPreview/>
-        <div className='flex h-16 items-center justify-center gap-3'>
-            <label className='flex items-center justify-center gap-2 font-medium'>
-                <input type='checkbox'
-                checked={isMicCamToggleOn}
-               onChange={()=>setIsMicCamToggleOn(e.target.checked)}
-               />
-               join with mic and camera off
-            </label>
-            <DeviceSettings/>
-
-        </div>
-        <Button className='rounded-md bg-green-500 px-4 py-2.5' onClick={()=>{
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+      <h1 className="text-center text-2xl font-bold">Setup</h1>
+      <VideoPreview />
+      <div className="flex h-16 items-center justify-center gap-3">
+        <label className="flex items-center justify-center gap-2 font-medium">
+          <input
+            type="checkbox"
+            checked={isMicCamToggled}
+            onChange={(e) => setIsMicCamToggled(e.target.checked)}
+          />
+          Join with mic and camera off
+        </label>
+        <DeviceSettings />
+      </div>
+      <Button
+        className="rounded-md bg-green-500 px-4 py-2.5"
+        onClick={() => {
           call.join();
-          setIsSetupComplete(true);
-        }}>
-          Join meeting 
-        </Button>
-      
-    </div>
-  )
-}
 
-export default MeetingSetup
+          setIsSetupComplete(true);
+        }}
+      >
+        Join meeting
+      </Button>
+    </div>
+  );
+};
+
+export default MeetingSetup;
